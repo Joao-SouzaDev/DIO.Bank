64ed3221094a7522b33de353ed6e590b868f7e34
Joao-SouzaDev <joao.souzadev@outlook.com>
2024-06-17 23:46:59 -0300

    desafio finalizado

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,24 @@
-interface ICard {
-  id: number,
-  paragraph: string,
-  details: string
-}
+import { Box, Button, Container, FormControl, Image, Input, Stack } from "@chakra-ui/react"
+import { login } from "../services/login"
 
-export const Card = ({ id, paragraph, details }: ICard) => {
-  return(
-    <div>
-      <h1>Card {id}</h1>
-      <p>{paragraph}</p>
-      <p>{details}</p>
-    </div>
+export const Card = () => {
+  return (
+    <Box minHeight='100vh' backgroundColor='#1E192C' padding='25vh' alignItems='center'>
+      <Container maxW='md' bg='white' padding={2} borderRadius={10}>
+        <Stack spacing={3} alignItems='center'>
+          <Box alignItems='center'>
+            <Image src="https://hermes.digitalinnovation.one/assets/diome/logo-full.svg" borderRadius='full'
+              boxSize='150px' />
+          </Box>
+          <FormControl>
+            <Input variant='filled' placeholder="Login" id="login-input" type="email" padding='5px' />
+          </FormControl>
+          <FormControl>
+            <Input variant='filled' placeholder="Password" id="pass-input" type="password" padding='5px' />
+          </FormControl>
+          <Button id="login-button" textColor='black' type='submit' onClick={login}>Entrar</Button>
+        </Stack>
+      </Container>
+    </Box>
   )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,13 @@
+import { Box, Flex, Heading, Spacer } from '@chakra-ui/react'
 import './Header.css'
 
-export const Header  = () => {
-  return(
-    <div className='header'>
-      Dio Bank
-    </div>
+export const Header = () => {
+  return (
+    <Flex minWidth='max-content' alignItems='center' gap='2' bg='#1E192C'>
+      <Box p='2'>
+        <Heading size='lg' color='powderblue'>Dio.Bank</Heading>
+      </Box>
+      <Spacer />
+    </Flex>
   )
 }
